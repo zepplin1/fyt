784d6fbb9716582abfd2cd9b12fea9919edff8ee
jmi0 <joseph.m.iannone@gmail.com>
2016-10-25 20:07:55 -0400

    update fyt.py
    commented

--- fytapp/fyt.py
+++ fytapp/fyt.py
@@ -2,14 +2,17 @@ from fytapp import app
 from flask import Flask, render_template
 from db import get_db
 
+# route http "/" to this
 @app.route('/')
 def index():
 	return 'homepage'
 
+# serve login.html to '/login'
 @app.route('/login')
 def login():
 	return render_template('login.html')
 
+# serve reg.html to '/register'
 @app.route('/register')
 def reg():
 	return render_template('reg.html')
